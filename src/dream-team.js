30e2461b8e65ee6dc39f4ad0f0707dadd18723df
Ivan <kan.ivann@gmail.com>
2020-03-22 18:35:48 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+  var res = [];
+  var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for (i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === "string") {
+      for (j=0; j < members[i].length; j++) {
+          for (t = 0; t < caps.length; t++){
+              members[i] = members[i].trim();
+              if (caps[t] == members[i][j]) {
+              res = res.concat(members[i][j].toUpperCase());
+          }}
+      break;
+      }}}
+  res.sort();
+  var b = res.join('');
+  return b;
+  }
+  else {
+    return false
+  }}
